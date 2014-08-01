02ae5fc0e1beeff95b388c0f903ff5cebc3a4b13
superhao <xiaohaoixiaoli@126.com>
2014-08-01 15:26:26 +0800

    modify

--- src/main/java/com/xiaohao/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by xiaohao on 2014/7/24.
- * 演示 如何发布和消费 服务     注册中心使用zookeeper 默认启动即可
- */
-package com.xiaohao;
--- src/main/java/com/xiaohao/serviceApi/DemoService.java
+++
@@ -1,9 +0,0 @@
-package com.xiaohao.serviceApi;
-
-/**
- * Created by xiaohao on 2014/7/23
- * dubbo service demo.
- */
-public interface DemoService {
-    String sayHello(String name);
-}
