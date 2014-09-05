00eb2a1999e1d9a6bf5b92a2cb93cd009794d912
superhao <zipu888@aliyun.com>
2014-09-05 10:03:13 +0800

    modify

---
+++ consumer/src/main/java/com/xiaohao/dubbodemo/consumer/webService/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.dubbodemo.xiaohao.com/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.xiaohao.dubbodemo.consumer.webService;
